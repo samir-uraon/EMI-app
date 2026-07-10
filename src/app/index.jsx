66bedd3014c7d070122e509e4938feb98a97246c
Your Name <pclenovo780@gmail.com>
2026-07-10 22:14:51 +0530

    new file:   .github/workflows/android.yml 	modified:   app.json 	deleted:    assets/images/android-icon-background.png 	deleted:    assets/images/android-icon-foreground.png 	deleted:    assets/images/android-icon-monochrome.png 	deleted:    assets/images/expo-badge-white.png 	deleted:    assets/images/expo-badge.png 	deleted:    assets/images/expo-logo.png 	deleted:    assets/images/favicon.png 	deleted:    assets/images/icon.png 	deleted:    assets/images/logo-glow.png 	new file:   assets/images/logo.png 	deleted:    assets/images/react-logo.png 	deleted:    assets/images/react-logo@2x.png 	deleted:    assets/images/react-logo@3x.png 	deleted:    assets/images/splash-icon.png 	deleted:    assets/images/tabIcons/explore.png 	deleted:    assets/images/tabIcons/explore@2x.png 	deleted:    assets/images/tabIcons/explore@3x.png 	deleted:    assets/images/tabIcons/home.png 	deleted:    assets/images/tabIcons/home@2x.png 	deleted:    assets/images/tabIcons/home@3x.png 	deleted:    assets/images/tutorial-web.png 	modified:   package-lock.json 	modified:   package.json 	new file:   src/app/_layout.jsx 	deleted:    src/app/_layout.tsx 	deleted:    src/app/explore.tsx 	new file:   src/app/index.jsx 	deleted:    src/app/index.tsx 	deleted:    src/components/animated-icon.module.css 	deleted:    src/components/animated-icon.tsx 	deleted:    src/components/animated-icon.web.tsx 	deleted:    src/components/app-tabs.tsx 	deleted:    src/components/app-tabs.web.tsx 	deleted:    src/components/external-link.tsx 	deleted:    src/components/hint-row.tsx 	deleted:    src/components/themed-text.tsx 	deleted:    src/components/themed-view.tsx 	deleted:    src/components/ui/collapsible.tsx 	deleted:    src/components/web-badge.tsx 	deleted:    src/constants/theme.ts 	deleted:    src/hooks/use-color-scheme.ts 	deleted:    src/hooks/use-color-scheme.web.ts 	deleted:    src/hooks/use-theme.ts 	modified:   tsconfig.json

---
+++ src/app/index.jsx
@@ -0,0 +1,45 @@
+import { View } from "react-native";
+import { WebView } from "react-native-webview";
+import * as Linking from "expo-linking";
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1}}>
+      <WebView
+        source={{ uri: "https://emi-goldy.vercel.app" }}
+        style={{ flex: 1, marginTop: 32  }}
+        javaScriptEnabled
+        domStorageEnabled
+        sharedCookiesEnabled
+        thirdPartyCookiesEnabled
+        onShouldStartLoadWithRequest={(request) => {
+          const url = request.url;
+
+          // Allow your own website inside WebView
+          if (url.startsWith("https://emi-goldy.vercel.app")) {
+            return true;
+          }
+
+          // Open external apps
+          if (
+            url.startsWith("tel:") ||
+            url.startsWith("mailto:") ||
+            url.startsWith("upi:") ||
+            url.startsWith("intent:") ||
+            url.startsWith("https://wa.me") ||
+            url.startsWith("https://api.whatsapp.com") ||
+            url.startsWith("https://maps.google.com") ||
+            url.startsWith("https://www.google.com/maps")
+          ) {
+            Linking.openURL(url);
+            return false;
+          }
+
+          // Open all other external links in browser
+          Linking.openURL(url);
+          return false;
+        }}
+      />
+    </View>
+  );
+}
